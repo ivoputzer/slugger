c45e0040a65675089da4abe9e7d8c2b5c9e0768c
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-06-11 18:32:30 +0200

    adds http wss server

---
+++ run.js
@@ -0,0 +1,36 @@
+
+const {createServer} = require('http')
+const {createServerFrom} = require('wss')
+
+const http = createServer(requestListener)
+const wss = createServerFrom(http, connectionListener)
+
+if (!module.parent) {
+  const {npm_package_config_http_port} = process.env
+  http.listen(npm_package_config_http_port, function () {
+    console.info('[web] listening', this.address())
+  })
+}
+
+function requestListener ({url, headers}, res) {
+  const {createReadStream} = require('fs')
+  const path = url.length === 1 ? '/index.html' : url
+  res.statusCode = 200
+  console.time(`[web] ${path}`)
+  createReadStream(`www${path}`)
+    .on('error', (err) => {
+      console.timeEnd(`[web] ${path}`)
+      console.error(`[web] %s`, err)
+      res.statusCode = 404
+      res.end()
+    })
+    .on('end', () => {
+      console.timeEnd(`[web] ${path}`)
+    })
+    .pipe(res)
+  function onError (err) {
+  }
+}
+function connectionListener (ws) {
+  console.log('[wss] connected: %d', this.clients.size)
+}
